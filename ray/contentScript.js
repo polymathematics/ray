d802ef54c661fc94ee8076a88beb14bd59f5053b
polymathematics <58536863+polymathematics@users.noreply.github.com>
2023-07-28 07:17:02 -0500

    Update contentScript.js
    added an image of Ray to Substack version and added some text styling

--- ray/contentScript.js
+++ ray/contentScript.js
@@ -16,18 +16,37 @@ const isWordpress = () => {
   const displayDontThinkTextSubstack = () => {
     console.log('we are on the editors')
     const dontThinkElement = document.createElement('div');
-    dontThinkElement.innerHTML = "<h2>Don't Think</h2>";
+    const rayImage = document.createElement('img');
+    const imageUrl = chrome.runtime.getURL('Bradbury.jpg');
+    rayImage.src = imageUrl;
+    rayImage.style.width = "165px";
+    rayImage.style.height = "145px"
+    console.log(imageUrl);
+    rayImage.style.position ="absolute";
+    rayImage.style.top = "65%";
+    rayImage.style.left = "3%";
+    dontThinkElement.innerHTML = "<h2 id='h2Text'>Don't Think</h2>";
+    dontThinkElement.style.position = "absolute";
+    dontThinkElement.style.top = "20%";
+    dontThinkElement.style.left = "5%";
+    dontThinkElement.style.zIndex = "9999";
     document.body.insertBefore(dontThinkElement, document.body.firstChild);
+    dontThinkElement.appendChild(rayImage);
+    const h2Text = document.getElementById('h2Text');
+    h2Text.style.color = "#686868";
   };
   
   const displayDontThinkTextWordpress = () => {
     console.log('we are on the editors');
     const dontThinkElement = document.createElement('div');
-    dontThinkElement.innerHTML = "<h2>Don't Think</h2>";
+    dontThinkElement.innerHTML = "<h2 id='h2Text'>Don't Think</h2>";
     dontThinkElement.style.position = "absolute";
     dontThinkElement.style.top = "20%";
+    dontThinkElement.style.left = "5%";
     dontThinkElement.style.zIndex = "9999";
     document.body.appendChild(dontThinkElement);
+    const h2Text = document.getElementById('h2Text');
+    h2Text.style.color = "#686868";
     };
 
   
